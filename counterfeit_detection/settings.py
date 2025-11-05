927a38742d203f6c24f9891178e82ce192058db8
mohansaivenkat <mohansaivenkat2004@gmail.com>
2025-11-06 01:23:40 +0530

    env updation in settings.py

--- counterfeit_detection/settings.py
+++ counterfeit_detection/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
